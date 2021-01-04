308a6f90c4fd649982baa1bb3822565f20b6432e
NicholasETerry <nicholas.e.terry@gmail.com>
2021-01-04 13:49:59 -0600

    changes to test methods

--- CustomListClass/CustomList.cs
+++ CustomListClass/CustomList.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace CustomListClass
 {
-    class CustomList
+    public class CustomList<T>
     {
+        List<int> testList = new List<int>();
+        public T[] holdingArray;
+        public T[] tempArray;
+        int j;
+        public CustomList()
+        {
+            j = 4;
+            holdingArray = new T[j]; // need a generic because a list could hold any object of the same type.
+        }
+        public void CustomAdd(T itemToAdd)
+        {
+            bool isNull = false;
+            while(isNull == false)
+            {
+                for (int i = 0; i < holdingArray.Length-1; i++)
+                {
+                    if(holdingArray[i] == null)
+                    {
+                        holdingArray[i] = itemToAdd;
+                        isNull = true;
+                    }
+                    else if(holdingArray[i] != null)
+                    {
+                        i++;
+                    }
+                    if(i == holdingArray.Length - 1)
+                    {
+                        tempArray = new T[j];
+                        for (int temp = 0; temp < holdingArray.Length - 1; i++)
+                        {
+                            tempArray[temp] = holdingArray[temp];
+                        }
+                        j *= 2;
+                        holdingArray = new T[j];
+                        for (int holding = 0; holding < tempArray.Length - 1; holding++)
+                        {
+                            holdingArray[holding] = tempArray[holding];
+                        }
+                        isNull = true;
+                    }
+                }
+            }
+        }
     }
+
 }
--- TestAddClass/TestAddMethod.cs
+++ TestAddClass/TestAddMethod.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClass;
+using System.Collections.Generic;
 
 namespace TestAddClass
 {
@@ -7,8 +9,20 @@ namespace TestAddClass
     public class TestAddMethod<T>
     {
         [TestMethod]
-        public void Test()
+
+        // As a developer, i want the ability to add an object to an instance of my custom-built list class by imitating the C# Add() method.
+        //-----
+        public void CustomAddMethod_IndexValueForAddedItem_ReturnFirstPositionInIndexValue(T testItem) // return index postion of item added - only adds one item to list
         {
+            // ARRANGE
+            CustomList<T> newCustomList = new CustomList<T>();
+            T expectedResult = testItem;
+            T actualResult;
+            // ACT
+            newCustomList.CustomAdd(testItem);
+            actualResult = newCustomList.holdingArray[0];
+            // ASSERT
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
