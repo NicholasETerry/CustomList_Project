9be421d0d8fb4c3fae6454127a3e3baf2de4d58b
NicholasETerry <nicholas.e.terry@gmail.com>
2021-01-04 12:28:51 -0600

    writting test methods for add methods in project class

---
+++ TestAddClass/TestAddMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAddClass
+{
+    [TestClass]
+    public class TestAddMethod<T>
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
